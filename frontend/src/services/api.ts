80ec5bf6213d27fd96a0ee0d25505be217434e0d
Jiten Basnet <jiten.basnet@ebpearls.com>
2025-08-06 17:24:33 +0545

    :up: update local url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -8,7 +8,7 @@ import {
   HealthResponse
 } from '../types/api';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://journey-ai-webservice.onrender.com';
+const API_BASE_URL =  'https://journey-ai-webservice.onrender.com'; //process.env.REACT_APP_API_URL ||
 
 // Create axios instance with default config
 const apiClient = axios.create({
